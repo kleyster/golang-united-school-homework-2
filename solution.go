64148118d3862a76b8b265f7ad95f3e0fa78aabc
Адильжан Аубакир <aubakirof@gmail.com>
2022-02-23 19:54:49 +0600

    d

--- solution.go
+++ solution.go
@@ -26,7 +26,7 @@ func CalcSquare(sideLen float64, sidesNum figurSide) float64 {
 		return float64(math.Pi*math.Pow(sideLen,2))
 	case SidesTriangle:
 		var height float64
-		height = math.Pow(sideLen,2)-math.Pow((sideLen/2),2)
+		height = sideLen* math.Sqrt(3)*2
 		return float64((sideLen*height)/2)
 	case SidesSquare:
 		return float64(math.Pow(sideLen,2))
